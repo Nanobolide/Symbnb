ce73b53111e940d3e67992d7f9f82c363b13f307
Nanobolide <maidougeoffroyjaphet@gmail.com>
2024-01-31 16:04:30 +0000

    Show annonce

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,10 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdController extends AbstractController
 {
     #[Route('/ads', name: 'ad_index')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(AdRepository $repo): Response
     {
         
-        $repo = $entityManager->getRepository(Ad::class);
+        //Dependance
+        // public function index( EntityManagerInterface $entityManager ): Response 
+        // $repo = $entityManager->getRepository(Ad::class);
+
         $ads = $repo->findAll();
 
 
@@ -22,6 +26,24 @@ class AdController extends AbstractController
         return $this->render('ad/index.html.twig', [
             'ads'=> $ads,
         ]);
+ 
+            }
+
         
-    }
+            // Permet d'afficher une seule Annonce
+        /**
+         * @return Response
+         */
+        #[Route('/ads/{slug}' , name: 'ads_show')]
+        public function show($slug , AdRepository $repo)
+        {
+            //Je récupère l'annonce qui correspond au slug !
+            $ad = $repo->findOneBySlug($slug);
+
+
+            return $this->render('ad/show.html.twig', [
+                    'ad' => $ad
+            ]);
+        }
 }
+
